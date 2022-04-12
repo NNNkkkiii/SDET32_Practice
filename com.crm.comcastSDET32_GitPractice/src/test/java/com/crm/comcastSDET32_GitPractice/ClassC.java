5f277260e22e90a1dba8e5cde5ce6e50ac1b87fc
nikki <nikki@LAPTOP-C1L05UUR>
2022-04-12 17:34:29 +0530

    commit 8

--- com.crm.comcastSDET32_GitPractice/src/test/java/com/crm/comcastSDET32_GitPractice/ClassC.java
+++ com.crm.comcastSDET32_GitPractice/src/test/java/com/crm/comcastSDET32_GitPractice/ClassC.java
@@ -11,10 +11,8 @@ public class ClassC
 		System.out.println("push2");
 		System.out.println("push3");
 		System.out.println("push4");
-        System.out.println("push6");
+     System.out.println("push6");
 		System.out.println("push7");
-		System.out.println("push8");
-		System.out.println("push9");
 
 		
 

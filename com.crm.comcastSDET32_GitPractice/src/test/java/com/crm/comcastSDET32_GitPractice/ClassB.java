df7fd321c5afa50d2fcffd514ba417305ba430f8
nikki <nikki@LAPTOP-C1L05UUR>
2022-04-12 12:21:37 +0530

    commit2

---
+++ com.crm.comcastSDET32_GitPractice/src/test/java/com/crm/comcastSDET32_GitPractice/ClassB.java
@@ -0,0 +1,13 @@
+package com.crm.comcastSDET32_GitPractice;
+
+import org.testng.annotations.Test;
+
+public class ClassB 
+{
+	@Test
+	public void demoTest1()
+	{
+	System.out.println("Push Second time");	
+	}
+
+}
